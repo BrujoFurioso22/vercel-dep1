29315b80ba142bde01d41c431da23cd52707be2a
Diego Barbecho <diegobarbecho133@gmail.com>
2024-04-16 00:13:44 -0500

    Funcionando API

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -2,8 +2,8 @@ const postgre = require('../database')
 const userController = {
     getAll: async(req, res) => {
         try {
-            const { rows } = await postgre.query("select * from users")
-            res.json({msg: "OK", data: rows})
+            const { rows } = await postgre.query("select * from users;")
+            res.json({msg: "OK", data: rows.data})
         } catch (error) {
             res.json({msg: error.msg})
         }
