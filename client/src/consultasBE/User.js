d6bc51c0f21df4520a5a22c375ba55980d115fa8
Diego Barbecho <diegobarbecho133@gmail.com>
2024-04-16 16:54:08 -0500

    cors

--- client/src/consultasBE/User.js
+++ client/src/consultasBE/User.js
@@ -4,7 +4,7 @@ import url from "./config/Url";
 export async function ObtenerUsuario() {
   try {
     const res = await axios.get(`${url}/api/users/`);
-    return res;
+    return res.data.data;
   } catch (err) {
     return err;
   }
