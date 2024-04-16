6899b12ec6450868006b570cbba8aab25d0ccf40
Diego Barbecho <diegobarbecho133@gmail.com>
2024-04-16 15:01:45 -0500

    Inicio de sesion y routes

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LadingPage';
 import Login from './pages/Login';
-import Admin from './components/Admin';
-import Seller from './components/Seller';
-import User from './components/User';
-import NotFound from './components/NotFound';
+// import Admin from './components/Admin';
+// import Seller from './components/Seller';
+// import User from './components/User';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
@@ -13,9 +13,9 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/admin/*" element={<Admin />} />
+        {/* <Route path="/admin/*" element={<Admin />} />
         <Route path="/seller/*" element={<Seller />} />
-        <Route path="/user/*" element={<User />} />
+        <Route path="/user/*" element={<User />} /> */}
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
