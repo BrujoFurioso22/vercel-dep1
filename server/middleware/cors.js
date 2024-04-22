e5378281ca47b2b21e58c1e2f8e9194e3ca01b3c
Diego Barbecho <diegobarbecho133@gmail.com>
2024-04-21 20:23:14 -0500

    Cors

--- server/middleware/cors.js
+++ server/middleware/cors.js
@@ -2,7 +2,7 @@ const cors = require("cors");
 
 const ACCEPTED_ORIGINS = [
   "https://vercel-dep1-client.vercel.app",
-  "http://localhost:3005",
+  // "http://localhost:3005",
 ];
 
 const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) =>
