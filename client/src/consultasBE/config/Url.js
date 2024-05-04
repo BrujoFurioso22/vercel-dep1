ad02e1255ee20992cde7cc6335381de4d9684b3c
Diego Barbecho <diegobarbecho133@gmail.com>
2024-05-03 20:00:24 -0500

    03/05/2024

--- client/src/consultasBE/config/Url.js
+++ client/src/consultasBE/config/Url.js
@@ -1,4 +1,4 @@
-// const url = process.env.REACT_APP_PRODUCTION_URL;
-const url = "http://localhost:5000";
+const url = process.env.REACT_APP_PRODUCTION_URL;
+// const url = "http://localhost:5000";
 
 export default url;
