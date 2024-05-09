162f26afc4cbe8315791e5e081e8d5f4ce8973c6
pedrof2311 <pedris2311@gmail.com>
2024-05-09 13:05:19 -0500

    09/05/2024

--- server/controllers/juego.controller.js
+++ server/controllers/juego.controller.js
@@ -240,55 +240,19 @@ export const juegoController = {
         Z: [1, 2, 3, 4, 5, 9, 13, 17, 21, 22, 23, 24, 25]
       };
       const arraysSeleccionados = letras.map(letra => data[letra]);
-      
+      console.log(arraysSeleccionados.length);
+      const { rows: rowstablas } = await pool.query("SELECT * FROM tablanormal;");
+        
+      console.log(rowstablas.length);
+      for(let i=0;i<arraysSeleccionados.length;i++){
+        for(let j=0;j<rowstablas.length;j++){
+          
+        }
+      }
 
     } catch (error) {
       console.error(error);
       res.status(500).json({ message: "An error occurred" });
     }
   },
-  // create: async(req, res) => {
-  //     try {
-  //         const { name, price } = req.body
-
-  //         const sql = 'INSERT INTO books(name, price) VALUES($1, $2) RETURNING *'
-
-  //         const { rows } = await postgre.query(sql, [name, price])
-
-  //         res.json({msg: "OK", data: rows[0]})
-
-  //     } catch (error) {
-  //         res.json({msg: error.msg})
-  //     }
-  // },
-  // updateById: async(req, res) => {
-  //     try {
-  //         const { name, price } = req.body
-
-  //         const sql = 'UPDATE books set name = $1, price = $2 where book_id = $3 RETURNING *'
-
-  //         const { rows } = await postgre.query(sql, [name, price, req.params.id])
-
-  //         res.json({msg: "OK", data: rows[0]})
-
-  //     } catch (error) {
-  //         res.json({msg: error.msg})
-  //     }
-  // },
-  // deleteById: async(req, res) => {
-  //     try {
-  //         const sql = 'DELETE FROM books where book_id = $1 RETURNING *'
-
-  //         const { rows } = await postgre.query(sql, [req.params.id])
-
-  //         if (rows[0]) {
-  //             return res.json({msg: "OK", data: rows[0]})
-  //         }
-
-  //         return res.status(404).json({msg: "not found"})
-
-  //     } catch (error) {
-  //         res.json({msg: error.msg})
-  //     }
-  // }
 };
