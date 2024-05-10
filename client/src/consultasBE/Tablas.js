9cade4be7295c3b6c569ed3594b8134471583a3e
Diego Barbecho <diegobarbecho133@gmail.com>
2024-05-10 13:35:19 -0500

    10/05/2024

--- client/src/consultasBE/Tablas.js
+++ client/src/consultasBE/Tablas.js
@@ -9,6 +9,103 @@ import url from "./config/Url";
 //     return err;
 //   }
 // }
+// export async function IngresarVenta(
+//   idvendedor,
+//   cccliente,
+//   nombrecliente,
+//   cantidadnormal,
+//   cantidadrapida,
+//   cantidaddinero,
+//   numerotransaccion
+// ) {
+//   try {
+//     function handlePartialResponse(response) {
+//       return new Promise((resolve, reject) => {
+//         const reader = response.body.getReader();
+
+//         function read() {
+//           reader
+//             .read()
+//             .then(({ done, value }) => {
+//               if (done) {
+//                 // Si la respuesta está completa, resolver la promesa
+//                 resolve();
+//                 return;
+//               }
+
+//               // Convertir el fragmento de la respuesta a texto
+//               const chunk = new TextDecoder("utf-8").decode(value);
+
+//               // Manejar la respuesta parcial, por ejemplo, actualizar la UI
+//               console.log(chunk);
+
+//               // Leer el próximo fragmento
+//               read();
+//             })
+//             .catch(reject);
+//         }
+
+//         read(); // Comenzar a leer la respuesta
+//       });
+//     }
+//     function insertDataToBackend(data) {
+//       return fetch(`${url}/api/tablas/insertarventa`, {
+//         method: "POST",
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//         body: data,
+//       });
+//     }
+//     function startInsertionProcess(data) {
+//       insertDataToBackend(data)
+//         .then((response) => {
+//           // Manejar la respuesta parcial del backend
+//           handlePartialResponse(response).then(() => {
+//             // Cuando se completa la respuesta parcial, mostrar mensaje de éxito o hacer otras acciones
+
+//             console.log("Proceso de inserción completado");
+//             return { ok: true };
+//           });
+//         })
+//         .catch((error) => {
+//           // Manejar errores de la solicitud
+//           console.error("Error al realizar la solicitud:", error);
+//           return { ok: false };
+//         });
+//     }
+//     startInsertionProcess({
+//       data: {
+//         idvendedor: idvendedor,
+//         cccliente: cccliente,
+//         nombrecliente: nombrecliente,
+//         cantidadnormal: cantidadnormal,
+//         cantidadrapida: cantidadrapida,
+//         cantidaddinero: cantidaddinero,
+//         numerotransaccion: numerotransaccion,
+//       },
+//     });
+//     // const res = await axios.post(
+//     //   `${url}/api/tablas/insertarventa`,
+//     //   {
+//     //     idvendedor: idvendedor,
+//     //     cccliente: cccliente,
+//     //     nombrecliente: nombrecliente,
+//     //     cantidadnormal: cantidadnormal,
+//     //     cantidadrapida: cantidadrapida,
+//     //     cantidaddinero: cantidaddinero,
+//     //     numerotransaccion: numerotransaccion,
+//     //   },
+//     //   { timeout: 30000 }
+//     // );
+//     // console.log(res);
+//     // return res;
+//   } catch (err) {
+//     return { ok: false };
+//   }
+// }
+
+
 export async function IngresarVenta(
   idvendedor,
   cccliente,
@@ -18,7 +115,9 @@ export async function IngresarVenta(
   cantidaddinero,
   numerotransaccion
 ) {
-  try {
+  try {// Reemplaza 'tu_url_base' con la URL de tu backend
+    
+    // Función para manejar la respuesta parcial del backend
     function handlePartialResponse(response) {
       return new Promise((resolve, reject) => {
         const reader = response.body.getReader();
@@ -48,63 +147,48 @@ export async function IngresarVenta(
         read(); // Comenzar a leer la respuesta
       });
     }
-    function insertDataToBackend(data) {
-      return fetch(`${url}/api/tablas/insertarventa`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: data,
-      });
-    }
-    function startInsertionProcess(data) {
-      insertDataToBackend(data)
-        .then((response) => {
-          // Manejar la respuesta parcial del backend
-          handlePartialResponse(response).then(() => {
-            // Cuando se completa la respuesta parcial, mostrar mensaje de éxito o hacer otras acciones
-
-            console.log("Proceso de inserción completado");
-            return { ok: true };
-          });
-        })
-        .catch((error) => {
-          // Manejar errores de la solicitud
-          console.error("Error al realizar la solicitud:", error);
-          return { ok: false };
+    
+    // Función para realizar la solicitud al backend
+    async function insertDataToBackend(data) {
+      try {
+        const response = await fetch(`${url}/api/tablas/insertarventa`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
         });
+
+        // Manejar la respuesta parcial del backend
+        await handlePartialResponse(response);
+
+        // Cuando se completa la respuesta parcial, mostrar mensaje de éxito o hacer otras acciones
+        console.log("Proceso de inserción completado");
+
+        return { ok: true };
+      } catch (error) {
+        // Manejar errores de la solicitud
+        console.error("Error al realizar la solicitud:", error);
+        return { ok: false };
+      }
     }
-    startInsertionProcess({
-      data: {
-        idvendedor: idvendedor,
-        cccliente: cccliente,
-        nombrecliente: nombrecliente,
-        cantidadnormal: cantidadnormal,
-        cantidadrapida: cantidadrapida,
-        cantidaddinero: cantidaddinero,
-        numerotransaccion: numerotransaccion,
-      },
+    
+    // Llamar a la función para realizar la inserción en el backend
+    return await insertDataToBackend({
+      idvendedor: idvendedor,
+      cccliente: cccliente,
+      nombrecliente: nombrecliente,
+      cantidadnormal: cantidadnormal,
+      cantidadrapida: cantidadrapida,
+      cantidaddinero: cantidaddinero,
+      numerotransaccion: numerotransaccion,
     });
-    // const res = await axios.post(
-    //   `${url}/api/tablas/insertarventa`,
-    //   {
-    //     idvendedor: idvendedor,
-    //     cccliente: cccliente,
-    //     nombrecliente: nombrecliente,
-    //     cantidadnormal: cantidadnormal,
-    //     cantidadrapida: cantidadrapida,
-    //     cantidaddinero: cantidaddinero,
-    //     numerotransaccion: numerotransaccion,
-    //   },
-    //   { timeout: 30000 }
-    // );
-    // console.log(res);
-    // return res;
+
   } catch (err) {
+    console.error("Error:", err);
     return { ok: false };
   }
 }
-
 export async function ConsultarVentas(idvendedor) {
   try {
     const res = await axios.post(`${url}/api/ventas/obtenerventasvendedor`, {
