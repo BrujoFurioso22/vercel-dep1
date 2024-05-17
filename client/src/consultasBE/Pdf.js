55da7e7e6abf6c30325b27491852666245f6e00d
pedrof2311 <pedris2311@gmail.com>
2024-05-17 15:07:41 -0500

    15:07p

--- client/src/consultasBE/Pdf.js
+++ client/src/consultasBE/Pdf.js
@@ -1,33 +1,45 @@
-import axiosInstance from './axiosConfig';
-
-export async function CrearPdf({ dataJuego1, dataJuego2, dataInfo1, dataInfo2, dato1 }) {
+import axios from "axios";
+import url from "./config/Url";
+export async function CrearPdf({
+  dataJuego1,
+  dataJuego2,
+  dataInfo1,
+  dataInfo2,
+  dato1
+}) {
   try {
-    const res = await axiosInstance.post(
-      '/api/pdf/generarPDF',
+    // console.log(dataJuego);
+    // console.log(dataInfo);
+    // console.log(nombreRes);
+    const res = await axios.post(
+      `${url}/api/pdf/generarPDF`,
       { dataJuego1, dataJuego2, dataInfo1, dataInfo2 },
-      { responseType: 'blob' }
+      { responseType: "blob" }
     );
-
+    // console.log(res);
+    // Comprobar el código de estado de la respuesta
     if (res.status === 205) {
       return false;
     } else if (res.status === 200) {
-      const blob = new Blob([res.data], { type: 'application/pdf' });
-      const url = window.URL.createObjectURL(blob);
+      // Crear un URL y descargar el PDF
+      const blob = new Blob([res.data], { type: "application/pdf" });
+      // Crear un URL para el Blob
+      const urll = window.URL.createObjectURL(blob);
+      // console.log(urll);
 
-      const nombreArchivo = `BINGO CHABELITA_${dato1}-${dataJuego1.length}-${dataJuego2.length}.pdf`;
-      const link = document.createElement('a');
-      link.href = url;
-      link.setAttribute('download', nombreArchivo);
+      let nombreArchivo = `BINGO CHABELITA_${dato1}-${dataJuego1.length}-${dataJuego2.length}.pdf`
+      const link = document.createElement("a");
+      link.setAttribute("href", urll); // o cualquier otro nombre de archivo
+      link.setAttribute("download", nombreArchivo); // o cualquier otro nombre de archivo
       document.body.appendChild(link);
       link.click();
       document.body.removeChild(link);
-      
       return true;
     }
 
+    // window.URL.revokeObjectURL(urll); // Limpia el objeto URL
     return res;
   } catch (err) {
-    console.error('Error al crear el PDF:', err);
     return err;
   }
 }
--- server/controllers/pdf.controller.js
+++ server/controllers/pdf.controller.js
@@ -1,136 +1,84 @@
 import { htmlTemplate1 } from "../components/template1.js";
-import { pool } from "../database.js";
-import { ServerStyleSheet } from "styled-components";
-import puppeteer from "puppeteer";
-import { minify } from "html-minifier";
 import { htmlTemplate2 } from "../components/template2.js";
-import { PDFDocument } from "pdf-lib";
+import { minify } from "html-minifier";
+import chromium from "chrome-aws-lambda";
 
 export const pdfController = {
   generatePDF: async (req, res) => {
+    let browser = null;
+    let page = null;
     try {
-      const { dataJuego1, dataJuego2, dataInfo1, dataInfo2 } =
-        req.body;
-      const browser = await puppeteer.launch({ headless: true });
-      const page = await browser.newPage();
+      const { dataJuego1, dataJuego2, dataInfo1, dataInfo2 } = req.body;
+
+      // Configuración de Puppeteer con chrome-aws-lambda
+      browser = await chromium.puppeteer.launch({
+        args: [...chromium.args, "--hide-scrollbars", "--disable-web-security"],
+        defaultViewport: chromium.defaultViewport,
+        executablePath: await chromium.executablePath,
+        headless: chromium.headless,
+        ignoreHTTPSErrors: true,
+      });
+
+      page = await browser.newPage();
       const pdfs = [];
-      let inicioHTML = ` <!DOCTYPE html>
-      <html lang="en">
-        <head>
-          <meta charset="UTF-8">
-          <meta name="viewport" content="width=device-width, initial-scale=1.0">
-          <style>
-          * {
-            box-sizing: border-box;
-          }
-          .contenedor-grid > span::after {
-              content: "";
-              margin-left: -10px;
-          }
-          .grid-cell:nth-child(5n) {
-            border-right: none; 
-          }
-          
-          .grid-cell:nth-child(-n + 5) {
-            border-top: none; 
-          }
-          
-          .grid-cell:nth-child(5n + 1) {
-            border-left: none;
-          }
-          
-          .grid-cell:nth-last-child(-n + 5) {
-            border-bottom: none; 
-          }
-          
-          .grid-cell.bottom-left {
-            border-radius: 0 0 0 15px;
-          }
-          
-          .grid-cell.bottom-right {
-            border-radius: 0 0 15px 0;
-          }
-  
-          .grid-cell-r:nth-child(3n) {
-            border-right: none;
-          }
-          .grid-cell-r:nth-child(-n + 3) {
-            border-top: none;
-          }
-          .grid-cell-r:nth-child(3n + 1) {
-            border-left: none;
-          }
-          .grid-cell-r:nth-last-child(-n + 3) {
-            border-bottom: none;
-          }
-          
-      </style>
-          <title>El Gran Bingo Chabelita</title>
-        </head>
-        <body style="width:100%;margin:0;display: flex;
-        justify-content: center;
-        align-items: center;flex-direction:column">`;
+      let inicioHTML = `<!DOCTYPE html>
+        <html lang="en">
+          <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <style>
+              * { box-sizing: border-box; }
+              .contenedor-grid > span::after { content: ""; margin-left: -10px; }
+              .grid-cell:nth-child(5n) { border-right: none; }
+              .grid-cell:nth-child(-n + 5) { border-top: none; }
+              .grid-cell:nth-child(5n + 1) { border-left: none; }
+              .grid-cell:nth-last-child(-n + 5) { border-bottom: none; }
+              .grid-cell.bottom-left { border-radius: 0 0 0 15px; }
+              .grid-cell.bottom-right { border-radius: 0 0 15px 0; }
+              .grid-cell-r:nth-child(3n) { border-right: none; }
+              .grid-cell-r:nth-child(-n + 3) { border-top: none; }
+              .grid-cell-r:nth-child(3n + 1) { border-left: none; }
+              .grid-cell-r:nth-last-child(-n + 3) { border-bottom: none; }
+            </style>
+            <title>El Gran Bingo Chabelita</title>
+          </head>
+          <body style="width:100%;margin:0;display: flex; justify-content: center; align-items: center;flex-direction:column">`;
       let htmls = inicioHTML;
 
-      async function generatePdfFromChunk(
-        dataJuego,
-        dataInfo,
-        htmlTemplateFunc
-      ) {
+      async function generatePdfFromChunk(dataJuego, dataInfo, htmlTemplateFunc) {
         if (dataJuego.length > 0) {
           for (const chunk of dataJuego) {
-            const htmlOrig = htmlTemplateFunc({
-              dataJuego: chunk,
-              dataInfo,
-            });
+            const htmlOrig = htmlTemplateFunc({ dataJuego: chunk, dataInfo });
             const htmlMinify = minify(htmlOrig, {
               removeComments: true,
               collapseWhitespace: true,
               minifyJS: true,
               minifyCSS: true,
             });
-            htmls = htmls + htmlMinify;
+            htmls += htmlMinify;
           }
-          // console.log(htmlMinify);
         }
       }
-      // console.log(dataJuego1);
-      // console.log(dataJuego2);
+
       if (dataJuego1.length === 0 && dataJuego2.length === 0) {
         return res.status(205).json({});
       } else {
         await generatePdfFromChunk(dataJuego1, dataInfo1, htmlTemplate1);
         await generatePdfFromChunk(dataJuego2, dataInfo2, htmlTemplate2);
-        htmls = htmls + `</body></html>`;
-        // pdfs.push(pdfBuffer);
+        htmls += `</body></html>`;
 
         await page.setContent(htmls, { waitUntil: "networkidle0" });
-        const pdfBuffer = await page.pdf({
-          format: "A4",
-          printBackground: true,
-        });
-        // const finalPdf = await combinePdfs(pdfs);
-        // const finalPdf = Buffer.concat(pdfs);
-        // console.log(finalPdf);
-        // await browser.close();
-        res.contentType("application/pdf");
+        const pdfBuffer = await page.pdf({ format: "A4", printBackground: true });
 
-        // res.setHeader("Content-Type", "application/pdf");
-        // res.setHeader(
-        //   "Content-Disposition",
-        //   'attachment; filename="combined.pdf"'
-        // );
-        // console.log(htmls);
+        res.contentType("application/pdf");
         return res.status(200).send(pdfBuffer);
       }
-
-      // res.send(pdfBuffer);
     } catch (error) {
       console.error("Error generating PDF", error);
       res.status(500).send("Error generating PDF");
     } finally {
-      // if (page) await page.close();
-      // if (browser) await browser.close();
+      if (page) await page.close();
+      if (browser) await browser.close();
     }
   },
 };
