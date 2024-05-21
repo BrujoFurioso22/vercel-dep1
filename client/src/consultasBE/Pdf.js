0c7d7ff4996e5c44cadc8b153fa392afd4f0c606
Diego Barbecho <diegobarbecho133@gmail.com>
2024-05-21 07:21:15 -0500

    21/05/2024

--- client/src/consultasBE/Pdf.js
+++ client/src/consultasBE/Pdf.js
@@ -5,41 +5,50 @@ export async function CrearPdf({
   dataJuego2,
   dataInfo1,
   dataInfo2,
-  dato1
+  dato1,
 }) {
   try {
-    // console.log(dataJuego);
-    // console.log(dataInfo);
-    // console.log(nombreRes);
     const res = await axios.post(
       `${url}/api/pdf/generarPDF`,
       { dataJuego1, dataJuego2, dataInfo1, dataInfo2 },
       { responseType: "blob" }
     );
-    // console.log(res);
-    // Comprobar el código de estado de la respuesta
     if (res.status === 205) {
       return false;
     } else if (res.status === 200) {
-      // Crear un URL y descargar el PDF
       const blob = new Blob([res.data], { type: "application/pdf" });
-      // Crear un URL para el Blob
       const urll = window.URL.createObjectURL(blob);
-      // console.log(urll);
 
-      let nombreArchivo = `BINGO CHABELITA_${dato1}-${dataJuego1.length}-${dataJuego2.length}.pdf`
+      // Sugerir un nombre de archivo
+      let nombreArchivoPredeterminado = `BINGO_CHABELITA_${dato1}-${dataJuego1.length}-${dataJuego2.length}.pdf`;
+
+      // Solicitar al usuario que verifique o modifique el nombre del archivo
+      let nombreArchivo = prompt(
+        "Por favor, ingrese el nombre del archivo para descargar:",
+        nombreArchivoPredeterminado
+      );
+
+      if (!nombreArchivo) {
+        // Si el usuario cancela el prompt, se detiene el proceso
+        return false;
+      }
+
+      // Asegurarse de que el nombre del archivo tenga la extensión .pdf
+      if (!nombreArchivo.toLowerCase().endsWith(".pdf")) {
+        nombreArchivo += ".pdf";
+      }
+
       const link = document.createElement("a");
-      link.setAttribute("href", urll); // o cualquier otro nombre de archivo
-      link.setAttribute("download", nombreArchivo); // o cualquier otro nombre de archivo
+      link.setAttribute("href", urll);
+      link.setAttribute("download", nombreArchivo);
       document.body.appendChild(link);
       link.click();
       document.body.removeChild(link);
       return true;
     }
 
-    // window.URL.revokeObjectURL(urll); // Limpia el objeto URL
-    return res;
+    return false;
   } catch (err) {
-    return err;
+    return false;
   }
 }
