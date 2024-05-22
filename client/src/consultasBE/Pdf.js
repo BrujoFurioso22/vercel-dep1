8908ce37a37937778556d4a16aa73f83f1abb287
Diego Barbecho <diegobarbecho133@gmail.com>
2024-05-22 08:21:49 -0500

    21/05/2024

--- client/src/consultasBE/Pdf.js
+++ client/src/consultasBE/Pdf.js
@@ -11,41 +11,27 @@ export async function CrearPdf({
     const res = await axios.post(
       `${url}/api/pdf/generarPDF`,
       { dataJuego1, dataJuego2, dataInfo1, dataInfo2 },
-      { responseType: "blob",  timeout: 500000 }
+      { responseType: "arraybuffer", timeout: 500000 }
     );
     if (res.status === 205) {
       return false;
     } else if (res.status === 200) {
       console.log(res);
       // console.log(res.data.html);
-      const blob = new Blob([res.data], { type: "application/pdf" });
-      const urll = window.URL.createObjectURL(blob);
-
-      // Sugerir un nombre de archivo
+      const contentType = res.headers["content-type"];
+      let fileExtension = "";
+      if (contentType === "application/pdf") {
+        fileExtension = "pdf";
+      } else if (contentType === "application/zip") {
+        fileExtension = "zip";
+      }
       let nombreArchivoPredeterminado = `BINGO_CHABELITA_${dato1}-${dataJuego1.length}-${dataJuego2.length}.pdf`;
 
-      // Solicitar al usuario que verifique o modifique el nombre del archivo
-      let nombreArchivo = prompt(
-        "Por favor, ingrese el nombre del archivo para descargar:",
-        nombreArchivoPredeterminado
-      );
-
-      if (!nombreArchivo) {
-        // Si el usuario cancela el prompt, se detiene el proceso
-        return true;
-      }
-
-      // Asegurarse de que el nombre del archivo tenga la extensión .pdf
-      if (!nombreArchivo.toLowerCase().endsWith(".pdf")) {
-        nombreArchivo += ".pdf";
-      }
-
+      const blob = new Blob([res.data], { type: contentType });
       const link = document.createElement("a");
-      link.setAttribute("href", urll);
-      link.setAttribute("download", nombreArchivo);
-      document.body.appendChild(link);
+      link.href = window.URL.createObjectURL(blob);
+      link.download = `${nombreArchivoPredeterminado}.${fileExtension}`;
       link.click();
-      document.body.removeChild(link);
       return true;
     }
 
