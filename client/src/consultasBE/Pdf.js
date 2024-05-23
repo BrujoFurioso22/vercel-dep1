7d9ef727709c7c43ea89acfa457a46be8f23b964
Diego Barbecho <diegobarbecho133@gmail.com>
2024-05-22 19:52:05 -0500

    21/05/2024

--- client/src/consultasBE/Pdf.js
+++ client/src/consultasBE/Pdf.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import url from "./config/Url";
+const ruta = "/pdfPreview.html";
 export async function CrearPdf({
   dataJuego1,
   dataJuego2,
@@ -11,7 +12,7 @@ export async function CrearPdf({
     const res = await axios.post(
       `${url}/api/pdf/generarPDF`,
       { dataJuego1, dataJuego2, dataInfo1, dataInfo2 },
-      { responseType: "blob", timeout: 500000 }
+      { responseType:"blob" , timeout: 500000 }
     );
     console.log(res);
     if (res.status === 205) {
@@ -19,20 +20,37 @@ export async function CrearPdf({
     } else if (res.status === 200) {
       console.log(res);
       // console.log(res.data.html);
-      const contentType = res.headers["content-type"];
-      let fileExtension = "";
-      if (contentType === "application/pdf") {
-        fileExtension = "pdf";
-      } else if (contentType === "application/zip") {
-        fileExtension = "zip";
-      }
+      // const contentType = res.headers["content-type"];
+      // let fileExtension = "";
+      // if (contentType === "application/pdf") {
+      //   fileExtension = "pdf";
+      // } else if (contentType === "application/zip") {
+      //   fileExtension = "zip";
+      // }
       let nombreArchivoPredeterminado = `BINGO_CHABELITA_${dato1}-${dataJuego1.length}-${dataJuego2.length}`;
 
-      const blob = new Blob([res.data], { type: contentType });
+      const blob = new Blob([res.data], { type: 'application/pdf' });
       const link = document.createElement("a");
       link.href = window.URL.createObjectURL(blob);
-      link.download = `${nombreArchivoPredeterminado}.${fileExtension}`;
+      link.download = `${nombreArchivoPredeterminado}.pdf`;
       link.click();
+      // const { html } = await res.data;
+
+      // const blob = new Blob([html], { type: "text/html" });
+      // const blobUrl = URL.createObjectURL(blob);
+
+      // const uniqueId = `pdfContent_${new Date().getTime()}`;
+      // const newWindow = window.open(
+      //   `${ruta}?blobUrl=${encodeURIComponent(blobUrl)}`,
+      //   "_blank"
+      // );
+      // console.log(html);
+      // // sessionStorage.setItem(uniqueId, html);
+      // // const newWindow = window.open(`${ruta}?id=${uniqueId}`, "_blank");
+
+      // if (!newWindow) {
+      //   console.error("Failed to open new window");
+      // }
       return true;
     }
 
